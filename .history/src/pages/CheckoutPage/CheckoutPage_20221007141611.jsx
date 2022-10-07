1d3874c2ff44b67fca51eb2b4bacbfccec9661a8
QuangDzu <nvu012030@gmail.com>
2022-10-07 16:55:47 +0700

    UP CHECKOUT (65%)

---
+++ .history/src/pages/CheckoutPage/CheckoutPage_20221007141611.jsx
@@ -0,0 +1,287 @@
+import { vnd } from "configs/functions";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./CheckoutPage.module.css";
+
+const CheckoutPage = ({ children }) => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const cartTotal = useSelector((state) => state.cart.cartTotal);
+  console.log(cartItems);
+
+  return (
+    <div className="flex flex-row flex-[1_0_auto] py-0 px-[5%] mx-auto w-[90%] max-w-screen-xl">
+      <div className="flex flex-column w-[62%] pr-[6%] pt-20">{children}</div>
+      <div className="sidebar w-[38%] relative bg-[#fafafa]">
+        <div className="block w-[300%] top-0 absolute bottom-0 -z-10 shadow-[1px_0_0_#e1e1e1_inset]"></div>
+        <div className="sidebar-content pl-[4%] pt-20">
+          <div className="content-item w-auto h-[65px] grid grid-cols-[65px_minmax(270px,_1fr)_100px] ">
+            <div className="product-img w-[65px] h-[65px] relative">
+              <div className="bg-[#d8d8d8] w-full h-full rounded-lg relative overflow-hidden">
+                <img
+                  src="https://product.hstatic.net/1000370235/product/z3733404419397_d753e3ea8a8f70f3d5acedb6b1a0deaa_5129de1d90b3454fa990016555e13999_small.jpg"
+                  alt=""
+                  className="object-cover w-full h-full p-2 "
+                />
+              </div>
+              <span className="absolute bg-[rgba(153,153,153,0.9)] z-50 px-3 py-2 text-xl quantity rounded-full -top-4 -right-4 text-white font-bold">
+                1
+              </span>
+            </div>
+            <div className="flex flex-col px-4 description">
+              <span className="text-2xl font-bold">Purple Jayden Dress</span>
+              <span>S</span>
+            </div>
+            <div className="right-0 flex items-center price">
+              <span className="font-semibold text-">590.000đ</span>
+            </div>
+          </div>
+          <div className="line border-[#e1e1e1] border-bottom my-4 border-x-0 border-t-0 w-full h-full"></div>
+        </div>
+        <div className="flex code">
+          <div className="relative code-input">
+            <label
+              htmlFor="discount-code"
+              className="top-0 z-30 block w-full px-4 mt-2 mb-3 transition-all ease-out delay-200 translate-y-2 pointer-events-none whitespace-nowrap text-ellipsis"
+            >
+              Mã giảm giá
+            </label>
+            <input
+              type="text"
+              className="w-full border shadow-md focus:ring-2 focus:outline-none focus:border-sky-500 focus:ring-sky-500 transition-all delay-300 bg-white text-[#333] rounded-md p-[10px_40px_10px_12px] mb-4"
+              placeholder="Số điện thoại"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    // <div className={styles["bao"]}>
+    //   <div className={styles["content"]}>
+    //     <div className={styles["wrap"]}>
+    //       <div className={styles["sidebar"]}>
+    //         <div className={styles["sidebar-content"]}>
+    //           <div
+    //             className={styles["order-summary order-summary-is-collapsed"]}
+    //           >
+    //             <h2 className={styles["visually-hidden"]}>
+    //               Thông tin đơn hàng
+    //             </h2>
+    //             <div className={styles["order-summary-sections"]}>
+    //               <div
+    //                 className={
+    //                   styles[
+    //                     "order-summary-section order-summary-section-product-list"
+    //                   ]
+    //                 }
+    //               >
+    //                 <table className={styles["product-table"]}>
+    //                   <tbody>
+    //                     {cartItems.length > 0 &&
+    //                       cartItems.map((product) => (
+    //                         <tr
+    //                           className={styles["product"]}
+    //                           data-product-id="1040710178"
+    //                           key={product.id}
+    //                         >
+    //                           <td className={styles["product-image"]}>
+    //                             <div className={styles["product-thumbnail"]}>
+    //                               <div
+    //                                 className={
+    //                                   styles["product-thumbnail-wrapper"]
+    //                                 }
+    //                               >
+    //                                 <img
+    //                                   className={
+    //                                     styles["product-thumbnail-image"]
+    //                                   }
+    //                                   alt={product.name}
+    //                                   src={product.thumbnail}
+    //                                 />
+    //                               </div>
+    //                               <span
+    //                                 className={
+    //                                   styles["product-thumbnail-quantity"]
+    //                                 }
+    //                                 aria-hidden="true"
+    //                               >
+    //                                 {product.quantity}
+    //                               </span>
+    //                             </div>
+    //                           </td>
+    //                           <td className={styles["product-description"]}>
+    //                             <span
+    //                               className={
+    //                                 styles[
+    //                                   "product-description-name order-summary-emphasis"
+    //                                 ]
+    //                               }
+    //                             >
+    //                               {product.name}
+    //                             </span>
+
+    //                             <span
+    //                               className={
+    //                                 styles[
+    //                                   "product-description-variant order-summary-small-text"
+    //                                 ]
+    //                               }
+    //                             >
+    //                               S
+    //                             </span>
+    //                           </td>
+    //                           <td
+    //                             className={
+    //                               styles["product-quantity visually-hidden"]
+    //                             }
+    //                           >
+    //                             1
+    //                           </td>
+    //                           <td className={styles["product-price"]}>
+    //                             <span
+    //                               className={styles["order-summary-emphasis"]}
+    //                             >
+    //                               {vnd(product.price)}
+    //                             </span>
+    //                           </td>
+    //                         </tr>
+    //                       ))}
+    //                   </tbody>
+    //                 </table>
+    //               </div>
+
+    //               <div
+    //                 className={
+    //                   styles[
+    //                     "order-summary-section order-summary-section-discount"
+    //                   ]
+    //                 }
+    //               >
+    //                 <form
+    //                   id="form_discount_add"
+    //                   acceptCharset="UTF-8"
+    //                   method="post"
+    //                 >
+    //                   <input name="utf8" type="hidden" value="✓" />
+    //                   <div className={styles["fieldset"]}>
+    //                     <div className={styles["field  "]}>
+    //                       <div className={styles["field-input-btn-wrapper"]}>
+    //                         <div className={styles["field-input-wrapper"]}>
+    //                           <label
+    //                             className={styles["field-label"]}
+    //                             htmlFor="discount.code"
+    //                           >
+    //                             Mã giảm giá
+    //                           </label>
+    //                           <input
+    //                             placeholder="Mã giảm giá"
+    //                             className={styles["field-input"]}
+    //                             size="30"
+    //                             type="text"
+    //                             id="discount.code"
+    //                             name="discount.code"
+    //                           />
+    //                         </div>
+    //                         <button
+    //                           type="submit"
+    //                           className={
+    //                             styles[
+    //                               "field-input-btn btn btn-default btn-disabled"
+    //                             ]
+    //                           }
+    //                         >
+    //                           <span className={styles["btn-content"]}>
+    //                             Sử dụng
+    //                           </span>
+    //                           <i
+    //                             className={
+    //                               styles["btn-spinner icon icon-button-spinner"]
+    //                             }
+    //                           ></i>
+    //                         </button>
+    //                       </div>
+    //                     </div>
+    //                   </div>
+    //                 </form>
+    //               </div>
+
+    //               <div
+    //                 className={
+    //                   styles[
+    //                     "order-summary-section order-summary-section-total-lines payment-lines"
+    //                   ]
+    //                 }
+    //               >
+    //                 <table className={styles["total-line-table"]}>
+    //                   <thead>
+    //                     <tr>
+    //                       <th scope="col">
+    //                         <span className={styles["visually-hidden"]}>
+    //                           Mô tả
+    //                         </span>
+    //                       </th>
+    //                       <th scope="col">
+    //                         <span className={styles["visually-hidden"]}>
+    //                           Giá
+    //                         </span>
+    //                       </th>
+    //                     </tr>
+    //                   </thead>
+    //                   <tbody>
+    //                     <tr
+    //                       className={styles["total-line total-line-subtotal"]}
+    //                     >
+    //                       <td className={styles["total-line-name"]}>
+    //                         Tạm tính
+    //                       </td>
+    //                       <td className={styles["total-line-price"]}>
+    //                         <span className={styles["order-summary-emphasis"]}>
+    //                           {vnd(cartTotal)}
+    //                         </span>
+    //                       </td>
+    //                     </tr>
+
+    //                     <tr
+    //                       className={styles["total-line total-line-shipping"]}
+    //                     >
+    //                       <td className={styles["total-line-name"]}>
+    //                         Phí vận chuyển
+    //                       </td>
+    //                       <td className={styles["total-line-price"]}>
+    //                         <span className={styles["order-summary-emphasis"]}>
+    //                           —
+    //                         </span>
+    //                       </td>
+    //                     </tr>
+    //                   </tbody>
+    //                   <tfoot className={styles["total-line-table-footer"]}>
+    //                     <tr className={styles["total-line"]}>
+    //                       <td
+    //                         className={
+    //                           styles["total-line-name payment-due-label"]
+    //                         }
+    //                       >
+    //                         <span className={styles["payment-due-label-total"]}>
+    //                           Tổng cộng
+    //                         </span>
+    //                       </td>
+    //                       <td className={styles["total-line-name payment-due"]}>
+    //                         {/* <span className={styles["payment-due-currency">VND</span> */}
+    //                         <span className={styles["payment-due-price"]}>
+    //                           {vnd(cartTotal)}
+    //                         </span>
+    //                       </td>
+    //                     </tr>
+    //                   </tfoot>
+    //                 </table>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+
+    //     </div>
+    //   </div>
+    // </div>
+  );
+};
+
+export default CheckoutPage;
---
+++ .history/src/pages/Login2/Login_20221007165024.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+import styles from "./Login.module.css";
+import {
+  createUser,
+  logInUser,
+  signInWithFacebook,
+  signInWithGoogle,
+} from "configs/auth";
+// ICONS
+import { BsFacebook } from "react-icons/bs";
+import { HiMail } from "react-icons/hi";
+import { GiPadlock } from "react-icons/gi";
+import { AiFillGooglePlusCircle } from "react-icons/ai";
+// ICONS
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import Input from "./components/input/Input";
+import Register from "./Register";
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .email(
+      // "Please enter valid email address"
+      "Vui lòng nhập địa chỉ email hợp lệ"
+    )
+    .required(
+      // "Please enter your email address"
+      "Hãy điền địa chỉ email của bạn"
+    ),
+  password: yup
+    .string()
+    .min(
+      8,
+      // "Your password must be at least 8 characters long or greater"
+      "Mật khẩu của bạn phải dài ít nhất 8 ký tự trở lên"
+    )
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      {
+        message:
+          // "Your password must have at least 8 characters, 1 uppercase, 1 lowercase, 1 special character"
+          "Mật khẩu của bạn phải có ít nhất 8 ký tự, 1 chữ hoa, 1 chữ thường, 1 ký tự đặc biệt",
+      }
+    )
+    .required("Vui lòng nhập mật khẩu"),
+});
+
+const Login = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting, isSubmitSuccessful },
+    setValue,
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+  console.log(isSubmitting);
+  console.log(errors);
+
+  const onSubmitLogin = (value) => {
+    if (!isValid) return;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+        console.log(value);
+      }, 5000);
+    });
+  };
+
+  return (
+    <div className={styles["wrapper"]}>
+      <div className="my-20 login-top">
+        <h2 className="mb-4 text-3xl font-light text-center">Đăng nhập bằng</h2>
+        <div className="flex flex-row items-center justify-center gap-x-4">
+          <button
+            className="w-full h-full rounded-3xl py-2 px-3 bg-[#546ea6] text-white flex items-center hover:bg-[#b2c9fb] transition-all"
+            onClick={signInWithFacebook}
+          >
+            <span className="mr-4">
+              <BsFacebook size="18"></BsFacebook>
+            </span>
+            Facebook
+          </button>
+          <button
+            className="w-full h-full rounded-3xl py-2 px-3 bg-[#df5656] text-white flex items-center hover:bg-[#ffb5b5] transition-all"
+            onClick={signInWithGoogle}
+          >
+            <span className="h-full mr-4">
+              <AiFillGooglePlusCircle size="18"></AiFillGooglePlusCircle>
+            </span>
+            Google
+          </button>
+        </div>
+      </div>
+      {/* =========================================== LINE =========================================== */}
+      <div className="relative border-[#dfdfdf] border-bottom">
+        <span className="absolute inline-block text-2xl text-[#999] bg-white p-2 -translate-x-2/4 left-2/4 -top-5 font-medium">
+          HOẶC
+        </span>
+      </div>
+      {/* =========================================== FORM =========================================== */}
+      <div className="flex flex-row justify-between">
+        <form
+          action=""
+          className="login-form col-sm-6 col-xs-12"
+          onSubmit={handleSubmit(onSubmitLogin)}
+        >
+          <h2 className="m-5 text-3xl font-light text-center">Đăng nhập</h2>
+          <div className="w-[410px] mx-auto my-0">
+            <div className="relative flex items-center ">
+              <span className="absolute top-3 right-1 text-[#ed1846] text-3xl z-1">
+                *
+              </span>
+              <span className="absolute inline-block top-2 left-3">
+                <HiMail size="22"></HiMail>
+              </span>
+              <Input
+                type="email"
+                control={control}
+                name="emaillogin"
+                placeholder="Nhập email của bạn"
+              ></Input>
+            </div>
+            {errors.emaillogin && (
+              <p className="mb-4 text-2xl text-red-500">
+                {errors.emaillogin.message}
+              </p>
+            )}
+
+            <div className="relative flex items-center ">
+              <span className="absolute top-3 right-1 text-[#ed1846] text-3xl z-1">
+                *
+              </span>
+              <span className="absolute inline-block top-2 left-3">
+                <GiPadlock size="22"></GiPadlock>
+              </span>
+              <Input
+                type="password"
+                control={control}
+                name="passwordlogin"
+                placeholder="Mật khẩu"
+              ></Input>
+            </div>
+            {errors.passwordlogin && (
+              <p className="mb-4 text-2xl text-red-500">
+                {errors.passwordlogin.message}
+              </p>
+            )}
+            <div className="relative flex flex-col items-center group ">
+              <button
+                className={`w-full h-[40px] mb-4 bg-slate-50 block border border-black rounded-lg hover:bg-slate-700 hover:text-white transition-all delay-100 leading-5 ${
+                  isSubmitting ? "opacity-50" : ""
+                }`}
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? (
+                  <div className="w-5 h-5 mx-auto border-2 border-t-2 border-white rounded-full border-t-transparent animate-spin"></div>
+                ) : (
+                  "Đăng nhập"
+                )}
+              </button>
+              <a href="sss" className="underline">
+                Quên mật khẩu?
+              </a>
+            </div>
+          </div>
+        </form>
+        {/* Đăng ký */}
+        <Register></Register>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
